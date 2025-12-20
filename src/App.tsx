fa393a86a7df67e0faf0af4e6a40213ab90ba106
GeoInf25 <michelotti002@gmail.com>
2025-12-20 22:18:01 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test AAAA HMR
+          Edit <code>src/App.tsx</code> and save to test ZZZZZZ HMR
         </p>
       </div>
       <p className="read-the-docs">
